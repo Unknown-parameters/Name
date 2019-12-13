43bc566817a35b158c6a5fc1f1ba53ae010f9c2f
Unknown-parameters <wangzhen2013@outlook.com>
2019-12-13 13:36:45 +0800

    change

--- draw_img.py
+++ draw_img.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import random
 #生成图片
-img = np.zeros([120, 400, 3], np.uint8)+255
+img = np.zeros([240, 800, 3], np.uint8)+255
 #生成随机数字,文字
 def v_code():
     res = []
@@ -15,20 +15,21 @@ def v_code():
     return ''.join(res),text
 #打印
 def print_chinese(img,num,text):
-    cv2.putText(img , num, (10, 40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
+    #cv2.putText(img , num, (10, 40), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 0), 3)
     cv2img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # cv2和PIL中颜色的hex码的储存顺序不同
     pilimg = Image.fromarray(cv2img)
     # PIL图片上打印汉字
     draw = ImageDraw.Draw(pilimg) # 图片上打印
-    font = ImageFont.truetype("simhei.ttf", 15, encoding="utf-8") # 参数1：字体文件路径，参数2：字体大小
-    draw.text((10, 60), text, (0, 0, 0), font=font) # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
+    font = ImageFont.truetype("simhei.ttf", 70, encoding="utf-8") # 参数1：字体文件路径，参数2：字体大小
+    draw.text((10, 20), num, (0, 0, 0), font=font)
+    draw.text((10, 100), text, (0, 0, 0), font=font) # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
     # PIL图片转cv2 图片
     img2 = cv2.cvtColor(np.array(pilimg), cv2.COLOR_RGB2BGR)
     return img2
 
 
 
-for i in range(100):
+for i in range(10):
 
     img1 = img.copy()
     num,text=v_code()
