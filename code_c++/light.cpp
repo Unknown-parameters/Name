e8c9df7e7a79e68ddc49722b26013b4d228d3501
Unknown-parameters <wangzhen2013@outlook.com>
2020-03-20 17:12:33 +0800

    Update light.cpp

--- code_c++/light.cpp
+++ code_c++/light.cpp
@@ -5,24 +5,24 @@ using namespace std;
 using namespace cv;
 
 
-bool adaptlight(Mat &src)
+bool adaptlight(Mat &scr)
 {
 	Mat gray;
 	Mat dst;
-	float weght;
-	cvtColor(src, gray, COLOR_RGB2GRAY);
+	double weght;
+	cvtColor(scr, gray, COLOR_RGB2GRAY);
 	equalizeHist(gray, dst);
-	for (int i = 0; i < src.rows; i++)
-		for (int j = 0; j < src.cols; j++) {
-			int  tmp_dst = dst.at<uchar>(i, j);
+	for (int i = 0; i < scr.rows; i++)
+		for (int j = 0; j < scr.cols; j++) {
+			int tmp_dst = dst.at<uchar>(i, j);
 			int tmp_gray = gray.at<uchar>(i, j);
 			if (tmp_gray != 0)
 				weght = tmp_dst / tmp_gray;
 			else
 				weght = 0;
 			for (int k = 0; k < 3; k++) {
-				int tmp = (uchar)src.at<Vec3b>(i, j)[k] * weght;
-				src.at<Vec3b>(i, j)[k] = tmp;
+				int tmp = (uchar)scr.at<Vec3b>(i, j)[k] * weght;
+				scr.at<Vec3b>(i, j)[k] = tmp;
 			}
 		}
 	return true;
@@ -34,11 +34,11 @@ bool adaptlight(Mat &src)
 int main()
 {
 	
-	Mat src = imread("D:/test.jpg");
-	Mat srcup = src.clone();
-	adaptlight(srcup);
-	imshow("yuantu", src);
-	imshow("junhenhua", srcup);
+	Mat scr = imread("D:/test.jpg");
+	Mat scrup = scr.clone();
+	adaptlight(scrup);
+	imshow("yuantu", scr);
+	imshow("junhenhua", scrup);
 	waitKey();
 
 	return 0;
